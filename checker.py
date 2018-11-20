02cfd652a948d20f1f337fb01fda1bdba7cedc95
Anders Fischer <andersfischern@me.com>
2018-11-20 11:50:15 +0100

    Implemented support for validating '#!' commands in top of scripts.

--- checker.py
+++ checker.py
@@ -3,30 +3,46 @@
 import bashlex
 import untangle
 import os
+import re
 import sys
 import melodic_default
 
 
 class MissingDependency:
-    def __init__(self, dependency, position):
+    def __init__(self, dependency, position, line_number=None):
         self.dependency = dependency
         self.position = position
-        self.line_number = None
+        self.line_number = line_number
 
 
 def setup(path):
-    script_pattern = 'grep -rl ' + path + r' -e "#\(\!\)\{0,1\}/bin/bash"'
+    grep_scripts = 'grep -rl ' + path + r' -e "#\(\!\)\{0,1\}/bin/bash"'
+    grep_shebang = 'grep -rl ' + path + \
+        r' -e "#\(\!\)\{0,1\}/bin\|usr\|sbin\|bash"'
     packages_pattern = 'find ' + path + r' -type f -name "package.xml"'
     scripts = list(
-        map(lambda l: l.strip(), os.popen(script_pattern).readlines()))
+        map(lambda l: l.strip(), os.popen(grep_scripts).readlines()))
+    shebangs = list(
+        map(lambda l: l.strip(), os.popen(grep_shebang).readlines()))
     packages_file = os.popen(packages_pattern).readlines()[0].strip()
     parsed = untangle.parse(packages_file)
-    run_deps = set(map(lambda e: e.cdata, parsed.package.run_depend))
-    all_deps = melodic_default.all_binaries.union(run_deps)
-    return (scripts, all_deps)
+    if not hasattr(parsed, 'package') or (not hasattr(parsed.package, 'run_depend') and not hasattr(parsed.package, 'exec_depend')):
+        return (scripts, melodic_default)
+
+    run_deps = set()
+    exec_deps = set()
+
+    if hasattr(parsed.package, 'run_depend'):
+        run_deps = set(map(lambda e: e.cdata, parsed.package.run_depend))
+
+    if hasattr(parsed.package, 'exec_depend'):
+        exec_deps = set(map(lambda e: e.cdata, parsed.package.exec_depend))
+
+    all_deps = melodic_default.all_binaries.union(run_deps).union(exec_deps)
+    return (scripts, shebangs, all_deps)
 
 
-def validate(scripts, run_deps):
+def validate(scripts, shebangs, deps):
     def validate_script(script):
         with open(script) as f:
             lines = f.readlines()
@@ -44,6 +60,16 @@ def validate(scripts, run_deps):
                 return None
             return (script, errors)
 
+    def validate_shebang(shebang):
+        with open(shebang) as f:
+            first = f.readlines()[:1][0].strip()
+            regexp = re.compile('((/| )([a-z])+)')
+            command, _, _ = regexp.findall(first)[-1:][0]
+            command = command[1:]
+            if command not in deps:
+                return (shebang, [MissingDependency(command, (first.rfind(command), len(first)), 1)])
+            return None
+
     def validate_line(line):
         ast = bashlex.parsesingle(line.strip())
         error = dependency_exists(ast)
@@ -54,19 +80,22 @@ def validate(scripts, run_deps):
             return None
 
         command = ast.parts[:1][0]
-        if command and command.kind is 'word' and command.word not in run_deps:
+        if command and command.kind is 'word' and command.word not in deps:
             return MissingDependency(command.word, command.pos)
         return None
 
-    missing = dict(
+    bash_missing = dict(
         filter(lambda r: r is not None,
                map(lambda s: validate_script(s), scripts)))
-    return missing
+
+    shebang_missing = dict(filter(lambda r: r is not None, map(
+        lambda s: validate_shebang(s), shebangs)))
+
+    return (bash_missing, shebang_missing)
 
 
 def print_result(result):
     if result:
-        print('Missing dependencies')
         for script, errors in result.items():
             print('in \'{}\''.format(script))
             if not errors:
@@ -75,13 +104,12 @@ def print_result(result):
                 for e in errors:
                     print('    ln: {} {} \t\t\t{}'.format(
                         e.line_number, e.position, e.dependency))
-    else:
-        print('No missing dependencies found.')
 
 
 if len(sys.argv) < 2:
     sys.exit('Please run the script with a path to a valid ROS package src.')
 
-scripts, run_deps = setup(sys.argv[1])
-res = validate(scripts, run_deps)
-print_result(res)
+scripts, shebangs, run_deps = setup(sys.argv[1])
+bash_scripts, shebang_scripts = validate(scripts, shebangs, run_deps)
+print_result(bash_scripts)
+print_result(shebang_scripts)

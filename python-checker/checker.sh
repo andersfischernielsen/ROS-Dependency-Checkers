9fef913b6a32791065ca1d648d6e4887826960db
Anders Fischer <andersfischern@me.com>
2019-03-20 16:12:23 +1300

    Added results. Added strict mode to checker.

--- python-checker/checker.sh
+++ python-checker/checker.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+strict=false
+if [ ! -z $2 ]; then 
+    strict=true
+    echo "Running in strict mode"
+fi
+
 checker_location=$(pwd)
 cd $1
 
@@ -9,9 +15,17 @@ find . -type f -iname "package.xml" -print0 | while IFS= read -r -d $'\0' line; 
     cat $checker_location/built-in_pip_packages.txt >> requirements.txt
 
     if [ "$(uname)" == "Darwin" ]; then
-        ggrep -F -v -f <(find . -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | ggrep -v 'src' | ggrep -v '\.') <(flake8 --select=I900 | ggrep -v 'msg')
+        if [ "$strict"==true ]; then 
+            ggrep -F -v -f <(find . -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | ggrep -v 'src' | ggrep -v '\.') <(flake8 --select=I900 | ggrep -v 'msg')
+        else
+            flake8 --select=I900 | ggrep -v 'msg'
+        fi
     else
-        grep -F -v -f <(find -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | grep -v 'src' | grep -v '\.') <(flake8 --select=I900 | grep -v 'msg')
+        if [ "$strict"==true ]; then 
+            grep -F -v -f <(find -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | grep -v 'src' | grep -v '\.') <(flake8 --select=I900 | grep -v 'msg')
+        else
+            flake8 --select=I900 | ggrep -v 'msg'
+        fi
     fi
 
     rm requirements.txt 

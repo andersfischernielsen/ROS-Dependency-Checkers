8c6d730e916db34dcc21c7286d5d8ae4f7862dce
Anders Fischer <andersfischern@me.com>
2019-03-27 19:04:47 +1300

    Added git history python script and some initial results for bash checker.

---
+++ bash-checker/Examples/rosdistro/check_git_history_for_all.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+for folder in [f.path for f in os.scandir('.') if f.is_dir()]:
+    folder = folder[2:]
+    history = True
+    with open(f"git_history_output_{folder}.txt", "w+", buffering=1) as file:
+        file.write(f'Checking {folder}...')
+        success = True
+        while success:
+            os.chdir(folder)
+            git = subprocess.call(
+                ['git', 'checkout', 'HEAD^'], stdout=file)
+            hash = subprocess.call(
+                ['git', 'rev-parse', 'HEAD'], stdout=file, stderr=file)
+            success = git == 0
+            os.chdir('..')
+            check = subprocess.call(['./checker.js', folder], stdout=file)

acd114161f7d46f8486b52f82227d335d8028534
Anders Fischer <andersfischern@me.com>
2019-05-08 15:24:48 +1200

    Added types, refactored slightly.

--- bash-checker/src/checker.ts
+++ bash-checker/src/checker.ts
@@ -6,44 +6,54 @@ import { defaults } from './melodic_default';
 import { parse as parseXML } from 'fast-xml-parser';
 import { shellSync } from 'execa';
 
-export class MissingDependency {
+type Name = { text: string; type: string };
+type Command = { type: string; name: Name; suffix: Name[] };
+type AST = { type: string; commands: Command[] };
+type Error = 'Error';
+type MissingDependency = Dependency | Error;
+type Result = { script: string; result: MissingDependency[] | Error };
+
+class Dependency {
   dependency: string;
   line: string;
-  constructor(c) {
+  constructor(c: Command) {
     this.dependency = c.name.text;
-    const l = c.suffix ? ' ' + c.suffix.map((s) => s.text).join(' ') : '';
+    const l = c.suffix ? c.suffix.reduce((i, n) => `${i} ${n.text}`, '') : '';
     this.line = l.length <= 40 ? l : `${l.substring(0, 40)}...`;
   }
 }
 
-const setup = (path: string) => {
-  const grep_scripts = `grep -rl --exclude=".travis.sh" --exclude-dir={.git,.travis} ${path} -e "#\\(\\!\\)\\{0,1\\}/bin/\\(bash\\|sh\\)"`;
-  const packages_pattern = `find ${path} -type f -name "package.xml"`;
+const getDependencies = (path: string) => {
+  const getDependency = (x: string) => {
+    const xml = readFileSync(x);
+    const parsed: string = parseXML(xml.toString());
+    if (parsed['package']['run_depend']) {
+      return parsed['package']['run_depend'];
+    }
+    if (parsed['package']['exec_depend']) {
+      return parsed['package']['exec_depend'];
+    }
+    return '';
+  };
+
+  const grepForScripts = `grep -rl --exclude=".travis.sh" --exclude-dir={.git,.travis} ${path} -e "#\\(\\!\\)\\{0,1\\}/bin/\\(bash\\|sh\\)"`;
+  const packagesPattern = `find ${path} -type f -name "package.xml"`;
   try {
-    const scripts = shellSync(grep_scripts).stdout.split('\n');
-    const packages_files = shellSync(packages_pattern).stdout;
-    const xmls = packages_files.split('\n').filter((l) => l !== '');
-    let deps = new Set<string>();
-    xmls.forEach((x) => {
-      const xml = readFileSync(x);
-      const parsed = parseXML(xml.toString());
-      if (parsed['package']['run_depend']) {
-        deps.add(parsed['package']['run_depend']);
-      }
-      if (parsed['package']['exec_depend']) {
-        deps.add(parsed['package']['exec_depend']);
-      }
-    });
-    const all_deps = new Set([...defaults, ...deps]);
-    return { scripts, deps: all_deps };
+    const scripts = shellSync(grepForScripts).stdout.split('\n');
+    const packageFiles = shellSync(packagesPattern).stdout;
+    const xmls = packageFiles.split('\n').filter((l) => l !== '');
+    const allDependencies = xmls.map(getDependency).filter((d) => d !== '');
+    let dependencies = new Set<string>(allDependencies);
+    const all_dependencies = new Set([...defaults, ...dependencies]);
+    return { scripts, dependencies: all_dependencies };
   } catch (error) {
     if (error.code !== 1) throw error;
-    return { scripts: [] as string[], deps: new Set() };
+    return { scripts: [] as string[], dependencies: new Set() };
   }
 };
 
-const validate = (scripts: string[], deps: Set<string>) => {
-  const validate = (contents) => {
+const validate = (scripts: string[], dependencies: Set<string>) => {
+  const validate = (contents: string) => {
     try {
       const ast = parse(contents);
       return dependencyExists(ast);
@@ -52,26 +62,28 @@ const validate = (scripts: string[], deps: Set<string>) => {
     }
   };
 
-  const dependencyExists = (ast) => {
+  const dependencyExists = (ast: AST) => {
     if (!ast) return [];
-    const commands = ast.commands.filter((c) => c.type && c.type === 'Command');
+    const commands: Command[] = ast.commands.filter(
+      (c: Command) => c.type && c.type === 'Command',
+    );
     const errors: MissingDependency[] = commands.map((c) => {
-      if (!c.name || !c.name.text) return undefined;
+      if (!c.name || !c.name.text) return 'Error';
       const isError =
         c.name.text &&
         !c.name.text.includes('./') &&
         !c.name.text.includes('$') &&
-        !deps.has(c.name.text) &&
+        !dependencies.has(c.name.text) &&
         !ast.commands.some(
-          (f) => f.type === 'Function' && f.name.text === c.name.text,
+          (f: Command) => f.type === 'Function' && f.name.text === c.name.text,
         );
-      if (!isError) return undefined;
-      return new MissingDependency(c);
+      if (!isError) return 'Error';
+      return new Dependency(c);
     });
-    return errors.filter((e) => e !== undefined);
+    return errors.filter((e) => e !== 'Error');
   };
 
-  const validateScript = (script: string) => {
+  const validateScript = (script: string): Result => {
     const contents = readFileSync(script).toString();
     try {
       if (
@@ -79,7 +91,7 @@ const validate = (scripts: string[], deps: Set<string>) => {
         (!contents.split('\n')[0].includes('bin/bash') &&
           !contents.split('\n')[0].includes('bin/sh'))
       ) {
-        return { script, result: undefined };
+        return { script, result: 'Error' };
       }
 
       const result = validate(contents);
@@ -90,27 +102,31 @@ const validate = (scripts: string[], deps: Set<string>) => {
   };
 
   const validated = scripts.map((s) => validateScript(s));
-  const actualScripts = validated.filter((s) => s.result !== undefined);
+  const actualScripts = validated.filter((s) => s.result !== 'Error');
   console.log(`Found ${actualScripts.length} bash scripts.`);
   const errors = validated.filter((o) => o && o.result && o.result.length);
   return errors;
 };
 
-const print_result = (result) => {
+const printResult = (result: Result[]) => {
   if (!result) return;
   result.forEach((r) => {
-    if (!r.result) return;
+    if (r.result === 'Error') return;
     console.log(`in '${r.script}'`);
-    r.result.forEach((res) => console.log(`\t\t${res.dependency}`));
+    r.result.forEach((res) => {
+      if (res === 'Error') return;
+      console.log(`\t\t${res.dependency}`);
+    });
   });
 };
 
 if (process.argv.length < 2) {
-  console.log('Please run the script with a path to a valid ROS package src.');
+  console.log('Please supply a path to a valid ROS package src.');
   process.exit(1);
 }
 
-const s = setup(process.argv[2]);
-const bash_scripts = validate(s.scripts, s.deps);
-console.log(`Found ${bash_scripts.length} possible bash script errors.`);
-print_result(bash_scripts);
+const s = getDependencies(process.argv[2]);
+const validated = validate(s.scripts, s.dependencies);
+if (validated.length <= 1) console.log(`Found 1 possible bash script error.`);
+else console.log(`Found ${validated.length} possible bash script errors.`);
+printResult(validated);

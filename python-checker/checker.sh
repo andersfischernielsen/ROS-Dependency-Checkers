2452563647bc3a9d26395e25d3f99abd3e5ebd0a
Anders Fischer <andersfischern@me.com>
2019-05-14 22:48:06 +1200

    Fixed error regarding strict mode.

--- python-checker/checker.sh
+++ python-checker/checker.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-strict=false
 if [ ! -z $2 ]; then 
     strict=true
-    echo "Running in strict mode"
 fi
 
 checker_location=$(pwd)
@@ -20,18 +18,20 @@ find . -type f -iname "package.xml" -print0 | while IFS= read -r -d $'\0' line; 
     cat $checker_location/built-in_pip_packages.txt >> requirements.txt
 
     if [ "$(uname)" == "Darwin" ]; then
-        if [ "$strict"==true ]; then 
-            ggrep -F -v -f <(find . -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | ggrep -v 'src' | ggrep -v '\.') <(flake8 --select=I900 | ggrep -v 'msg')
+        if [ "$strict" == true ]; then 
+     		echo "Running in strict mode"
+			ggrep -F -v -f <(find . -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | ggrep -v 'src' | ggrep -v '\.') <(flake8 --select=I900 | ggrep -v 'msg')
         else
             flake8 --select=I900 | ggrep -v 'msg'
         fi
     else
-        if [ "$strict"==true ]; then 
+        if [ "$strict" == true ]; then 
+        	echo "Running in strict mode"
             grep -F -v -f <(find -type f -name "*.py" | sed 's|/[^/]*$||' | sed 's|.*/||' | grep -v 'src' | grep -v '\.') <(flake8 --select=I900 | grep -v 'msg')
         else
             flake8 --select=I900 | ggrep -v 'msg'
         fi
     fi
 
-    rm requirements.txt 
+	rm requirements.txt     
 done
